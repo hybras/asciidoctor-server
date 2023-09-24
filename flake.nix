acd6245589dc7ff7a99c93e855bf4dbe2b1124e5
hybras <24651269+hybras@users.noreply.github.com>
2023-09-24 12:02:16 -0400

    Try to install gems through nx

--- flake.nix
+++ flake.nix
@@ -5,15 +5,25 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
     rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils.url = "github:numtide/flake-utils";
+    ruby-nix.url = "github:inscapist/ruby-nix";
+    ruby-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+  outputs = { self, nixpkgs, rust-overlay, ruby-nix, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        overlays = [ (import rust-overlay) ];
+        overlays = [ 
+          (import rust-overlay) 
+          ruby-nix.overlays.ruby ];
         pkgs = import nixpkgs {
           inherit system overlays;
         };
+        rubyNix = ruby-nix.lib pkgs;
+        inherit (rubyNix {
+          name = "asciidoctor-server";
+          gemset = ./server-rb/gemset.nix;
+        })
+          env ruby;
       in
       {
         devShells.default = pkgs.mkShell {
@@ -22,7 +32,9 @@
             {
               extensions = [ "rust-src" "rust-analyzer" ];
             })
-            pkgs.ruby
+            # pkgs.ruby
+            ruby
+            env
             pkgs.rubyPackages.solargraph
             pkgs.protobuf
             pkgs.rosie
