a31881c24d7b17eb5ec4f3f6cf9da267a5e63178
hybras <24651269+hybras@users.noreply.github.com>
2023-09-27 15:46:44 -0400

    working go client stub
    sends a single hardcoded request

---
+++ client-go/main.go
@@ -0,0 +1,45 @@
+// Package main implements a client for Greeter service.
+package main
+
+import (
+	"context"
+	// "flag"
+	"log"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	pb "io.github.hybras/asciidoctor-server/proto"
+)
+
+// var (
+// 	addr = flag.String("addr", "unix://.asciidoctor-server", "the address to connect to")
+// 	backend = flag.String("backend", "html5", "backend")
+// )
+
+func main() {
+	// flag.Parse()
+	// Set up a connection to the server.
+	conn, err := grpc.Dial("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewAsciidoctorConverterClient(conn)
+
+	// Contact the server and print out its response.
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+		Extensions: []string{},
+		Backend:    "html5",
+		Attributes: []string{},
+		Input: "\n" +
+			"= Asciidoctor Hello\n" +
+			"\n" +
+			"Paragraph\n",
+	})
+	if err != nil {
+		log.Fatalf("could not convert: %v", err)
+	}
+	log.Printf("Output:\n%s", r.GetOutput())
+}
