8f3f0f2d4a552e97caef533372bd786769775d9e
hybras <24651269+hybras@users.noreply.github.com>
2023-09-27 20:42:28 -0400

    regenerate go protobuf code

--- proto/asciidoctor_grpc.pb.go
+++ proto/asciidoctor_grpc.pb.go
@@ -2,7 +2,7 @@
 // versions:
 // - protoc-gen-go-grpc v1.2.0
 // - protoc             v3.21.12
-// source: asciidoctor.proto
+// source: proto/asciidoctor.proto
 
 package proto
 
@@ -101,5 +101,5 @@ var AsciidoctorConverter_ServiceDesc = grpc.ServiceDesc{
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
-	Metadata: "asciidoctor.proto",
+	Metadata: "proto/asciidoctor.proto",
 }
