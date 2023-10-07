946c7d7f6ef58cbc9be6b0927c8ff13dc9c3d478
hybras <24651269+hybras@users.noreply.github.com>
2023-10-07 14:55:42 -0400

    Move serve binary to proper location

--- server-rb/lib/asciidoctor-server.rb
+++ server-rb/lib/asciidoctor-server.rb
@@ -2,21 +2,6 @@
 
 require_relative "asciidoctor-server/version"
 require_relative "asciidoctor_services_pb"
-require "asciidoctor"
-require "logger"
-require "grpc"
-
-module MyLogger
-  LOGGER = Logger.new $stderr, level: Logger::INFO
-  def logger
-    LOGGER
-  end
-end
-
-# Define a gRPC module-level logger method before grpc/logconfig.rb loads.
-module GRPC
-  extend MyLogger
-end
 
 module Asciidoctor
   module Server
@@ -34,17 +19,3 @@ module Asciidoctor
     end
   end
 end
-
-def main
-  addr = 'unix:///Users/hybras/Documents/asciidoctor-server/socket.sock'
-  # addr = "localhost:50051"
-  s = GRPC::RpcServer.new
-  s.add_http2_port(addr, :this_port_is_insecure)
-  s.handle(Asciidoctor::Server::AsciidoctorServer)
-  # Runs the server with SIGHUP, SIGINT and SIGTERM signal handlers to
-  #   gracefully shutdown.
-  # User could also choose to run server via call to run_till_terminated
-  s.run_till_terminated_or_interrupted([1, 'int', 'SIGTERM'])
-end
-
-main
