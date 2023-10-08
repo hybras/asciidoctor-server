5e2ff4f36a2dc69071ad69b903ed2e4614345678
hybras <24651269+hybras@users.noreply.github.com>
2023-10-08 12:54:49 -0400

    refactor server cli to its own module

---
+++ server-rb/lib/asciidoctor-server/cli.rb
@@ -0,0 +1,28 @@
+require "grpc"
+require "optparse"
+
+module Asciidoctor
+  module Server
+    module Cli
+      Options = Struct.new(:thread_pool_size, :address) do
+        def self.parse(args)
+          Options.new(
+            ::GRPC::RpcServer::DEFAULT_POOL_SIZE,
+            "unix://#{Pathname.pwd / ".asciidoctor-server.sock"}"
+          ).parse(args)
+        end
+        def parse(args)
+          positional_args = OptionParser.new do |parser|
+            parser.on("-addr ADDRESS", "--address ADDRESS")
+            parser.on("-t THREAD_POOLSIZE", "--pool-size THREAD_POOLSIZE", Integer, "thread pool size")
+            parser.on("-h", "--help", "print help") do
+              puts parser
+              exit
+            end
+          end.parse(args, into: self)
+          return self
+        end
+      end
+    end
+  end
+end
