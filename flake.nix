892609f7c4530c1899f7e1a6cfb312aefb950bd9
hybras <24651269+hybras@users.noreply.github.com>
2023-12-17 20:54:17 -0500

    Add rust package to flake

--- flake.nix
+++ flake.nix
@@ -1,22 +1,36 @@
 {
-  description = "A devShell example";
-
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.flake-utils.follows = "flake-utils";
     };
     flake-utils.url = "github:numtide/flake-utils";
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+  outputs = { self, nixpkgs, crane, rust-overlay, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
           overlays = [ (import rust-overlay) ];
+        };
+        rust-toolchain = (pkgs.rust-bin.stable.latest.default.override
+                {
+                  extensions = [ "rust-src" "rust-analyzer" ];
+                });
+        craneLib = (crane.mkLib pkgs).overrideToolchain rust-toolchain;
+        asciidoctor-client = craneLib.buildPackage {
+          src = craneLib.cleanCargoSource (craneLib.path ./client-rs);
+          strictDeps = true;
+          buildInputs = [
+            pkgs.protobuf
+          ];
         };
       in
       {
@@ -27,11 +41,12 @@
               pkgs.rubyPackages.solargraph
               pkgs.protobuf
               pkgs.cargo-release
-              (pkgs.rust-bin.stable.latest.default.override
-                {
-                  extensions = [ "rust-src" "rust-analyzer" ];
-                })
+              rust-toolchain
             ];
+        };
+        packages.asciidoctor-client = asciidoctor-client;
+        apps.asciidoctor-client = flake-utils.lib.mkApp {
+          drv = asciidoctor-client;
         };
       }
     );
