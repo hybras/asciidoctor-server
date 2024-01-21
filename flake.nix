7753cc343fd092e31a080064c2086cab03d05ec5
hybras <24651269+hybras@users.noreply.github.com>
2024-01-20 19:50:30 -0500

    Upgrade nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
